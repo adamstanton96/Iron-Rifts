dff553daa85ae9b1390440ab325f240ee2d7c1df
Adam Stanton <a.stanton.1996@gmail.com>
2018-03-07 09:19:32 +0000

    Updated -still incomplete- physics system classes

--- blue-iron-greyhound/IronRiftsPhysicsSystem.h
+++ blue-iron-greyhound/IronRiftsPhysicsSystem.h
@@ -1,1 +1,33 @@
 #pragma once
+#include "PhysicsSystem.h"
+
+class IronRiftsPhysicsSystem : public PhysicsSystem
+{
+public:
+	IronRiftsPhysicsSystem();
+	virtual ~IronRiftsPhysicsSystem() {}
+
+	void init();
+
+	void addStaticBody(RigidBodyComponent* rigidbody);
+	void addDynamicBody(RigidBodyComponent* rigidbody);
+
+	//Collision Detection Using RigidBody Components...
+	void collisionCheck(RigidBodyComponent* rigidbody);
+	void collisionReaction(RigidBodyComponent* rigidbody);
+	void displacementReaction(RigidBodyComponent* rigidbody, glm::vec3 displacementVector);
+
+	//Raycasting...
+	Ray castRay();
+	std::vector<GameObject*> checkRayCollision(Ray ray);
+	GameObject* checkClosest(std::vector<GameObject*>);
+
+private:
+	bool AABBtoAABB(AABB*, AABB*);
+	bool OBBtoOBB(OBB* box1, OBB* box2, RigidBodyComponent* rigidbody);
+	bool OBBtoAABB(OBB* box1, AABB* box2);
+
+	std::vector<RigidBodyComponent*> staticBodies;
+	std::vector<RigidBodyComponent*> dynamicBodies;
+
+};
--- blue-iron-greyhound/PhysicsSystem.h
+++ blue-iron-greyhound/PhysicsSystem.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+#include "GameObject.h"
+#include "System.h"
+#include "AABB.h"
+#include "OBB.h"
+
+struct Ray { glm::vec3 startPoint, endPoint; };
+
+class RigidBodyComponent;
+
+class PhysicsSystem : public System
+{
+public:
+	PhysicsSystem();
+	virtual ~PhysicsSystem() {}
+
+	void init();
+
+	void addStaticBody(RigidBodyComponent* rigidbody);
+	void addDynamicBody(RigidBodyComponent* rigidbody);
+
+	void collisionCheck(RigidBodyComponent* rigidbody);
+	void collisionReaction(RigidBodyComponent* rigidbody);
+	void displacementReaction(RigidBodyComponent* rigidbody, glm::vec3 displacementVector);
+};
